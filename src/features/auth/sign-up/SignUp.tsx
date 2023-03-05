78fa59a508cd661b0928d141599b945297672564
Ateo101 <nikmail242@gmail.com>
2023-03-05 19:19:15 -0300

    fix: css & label bug

--- src/features/auth/sign-up/SignUp.tsx
+++ src/features/auth/sign-up/SignUp.tsx
@@ -18,8 +18,6 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff'
 
 import auth from '../auth.module.css'
 
-import s from './signUp.module.css'
-
 import { NavLink } from 'react-router-dom'
 
 export const SignUp = () => {
@@ -32,52 +30,52 @@ export const SignUp = () => {
   }
 
   return (
-    <Grid container spacing={2} className={auth.container}>
-      <form className={auth.form}>
-        <FormControl className={auth.formControl} variant="standard">
-          <FormGroup className={auth.formGroup}>
-            <h1 className={auth.h1}>Sign Up</h1>
-            <TextField label="Email" variant="standard" className={auth.input} />
-            <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-            <Input
-              id="standard-adornment-password"
-              className={auth.input}
-              type={showPassword ? 'text' : 'password'}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-            <InputLabel htmlFor="standard-adornment-password">Confirm password</InputLabel>
-            <Input
-              id="standard-adornment-password"
-              className={auth.input}
-              type={showPassword ? 'text' : 'password'}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-            <Button variant="contained" className={auth.button}>
-              Sign Up
-            </Button>
-          </FormGroup>
-        </FormControl>
-      </form>
+    <Grid container className={auth.container}>
+      <h1 className={auth.h1}>Sign Up</h1>
+      <FormControl variant="standard" className={auth.formControl}>
+        <TextField label="Email" variant="standard" />
+      </FormControl>
+      <FormControl variant="standard" className={auth.formControl}>
+        <InputLabel>Password</InputLabel>
+        <Input
+          id="standard-adornment-password"
+          className={auth.input}
+          type={showPassword ? 'text' : 'password'}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <FormControl variant="standard" className={auth.formControl}>
+        <InputLabel className={auth.r}>Password</InputLabel>
+        <Input
+          id="standard-adornment-password"
+          className={auth.input}
+          type={showPassword ? 'text' : 'password'}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <Button variant="contained" className={auth.button}>
+        Sign Up
+      </Button>
       <p className={auth.haveAccText}>Already have an account?</p>
       <NavLink to={`/fridayProject/login`}>
         <p className={auth.haveAccLink}>Sign In</p>
