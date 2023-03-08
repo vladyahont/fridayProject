3ef4df57575a1f2f109b3a70a4cdc2cfe4828d84
AgniVon1 <iliatron00@gmail.com>
2023-03-08 22:48:09 +0300

    feat: useAppDispatch,useAppSelector

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
-import { applyMiddleware, combineReducers, legacy_createStore } from 'redux'
-import thunk, {ThunkAction} from 'redux-thunk'
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'redux'
+import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk'
 
 import {AuthActionsType, authReducer} from '../features/auth/auth-reducer'
 
 import {AppActionsType, appReducer} from './app-reducer'
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 
 const rootReducer = combineReducers({
   app: appReducer,
@@ -15,5 +16,15 @@ export type AppStateType = ReturnType<typeof rootReducer>
 
 export type RootActionType = AppActionsType | AuthActionsType
 
-export type RootThunkType<ReturnType = void> = ThunkAction<ReturnType, AppStateType, unknown, RootActionType>
+export type RootThunkType<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppStateType,
+  unknown,
+  RootActionType>
+
+
+export type AppThunkDispatch = ThunkDispatch<AppStateType, any, AnyAction>
+
+export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
+export const useAppSelector: TypedUseSelectorHook<AppStateType> = useSelector
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
