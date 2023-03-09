b3d6ac84477da12ece0fb4150d9ae57bc13511cd
My Name <vlad.yahont.98@mail.ru>
2023-03-09 20:21:06 +0300

    fix Login

--- src/app/Path.ts
+++ src/app/Path.ts
@@ -1,10 +1,8 @@
 export const PATH = {
-  PROFILE: "/profile",
-  REGISTRATION: "/registration",
-  LOGIN:"/login",
-  404:"/404",
-  RECOVERY:"/forgot",
-  NEWPASSWORD:"/newPassword"
-
-  } as const
-
+  PROFILE: '/profile',
+  REGISTRATION: '/registration',
+  LOGIN: '/login',
+  404: '/404',
+  RECOVERY: '/forgot',
+  NEWPASSWORD: '/newPassword',
+} as const
