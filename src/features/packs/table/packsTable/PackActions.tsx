cced61723d55efa67aff0bf992dc7306df402c1f
AgniVon1 <iliatron00@gmail.com>
2023-04-01 13:32:10 +0300

    ref:Body and Head table

---
+++ src/features/packs/table/packsTable/PackActions.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {
+  packName?:string,
+  idUser:string,
+}
+export const PackActions = ({packName,idUser}:Props) => {
+  return (
+    <div>
+      {/*idUser !== myID*/}
+    </div>
+  );
+};
+
